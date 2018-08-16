e6601d2eb093a8663ecd5cac65911254249e7e59
Alina Walling <alina.walling@duke.edu>
2018-08-16 19:55:22 -0400

    Using webpack simple

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import VueFire from "vuefire"
-import App from './App'
+import App from './App.vue'
 
-Vue.use(Vuetify)
-Vue.use(VueFire)
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
