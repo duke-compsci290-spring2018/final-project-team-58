90dfbde0f17c65d595fc62169a2d4c993d91f244
Alina Walling <alina.walling@duke.edu>
2018-09-25 23:02:40 -0400

    Updated babel preset env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // URL for find back end's API
-export const API_URL = 'http://localhost:3000'
-//export const API_URL = 'https://compsci-290-final-project.herokuapp.com'
+//export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://compsci-290-final-project.herokuapp.com'
